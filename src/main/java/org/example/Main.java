4bdafb289df5ff6f958f3c8ff5e45e1fe5b4de71
romanchervakov <61947236+romanchervakov@users.noreply.github.com>
2022-11-18 04:05:49 +0300

    Delete src/main/java/org/example directory

--- src/main/java/org/example/Main.java
+++
@@ -1,6 +0,0 @@
-package org.example;
-public class Main {
-             public static void main(String[] args) {
-                                     Calculator A = new Calculator();
-             }
-}
